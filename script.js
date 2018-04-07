0560cbfd9cc474e2357d52d18e4cf4e8366eeb64
dsdunn <dsdunnmusic@gmail.com>
2018-04-07 14:15:49 -0600

    Rename functions and refactor delete function

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ $ideaBody = $('.input-body');
 $ideaSave = $('.save-button');
 $ideaSearch = $('.search');
 $anchor = $('.card-section');
-var quality = 0;
 recreateCards();
 
-$ideaSave.on('click', validateInput);
+$ideaSave.on('click', newIdea);
 $ideaTitle.on('keyup', toggleButton);
 $ideaBody.on('keyup', toggleButton);
 
 function toggleButton () {
+  $ideaTitle = $('.input-title');
   if ($ideaTitle.val() === "" && $ideaBody.val() === "") {
     $ideaSave.prop("disabled", true);
   } else {
@@ -18,15 +18,16 @@ function toggleButton () {
   }
 }
 
-function validateInput () {
-  if ($ideaTitle.val() === "" || $ideaBody.val() === "") {
-    alert("Please Enter Fillout The Title And Body Fields");
-  } else {
-    userInput();
-  }
-}
+// function validateInput () {
+//   $ideaTitle = $('.input-title');
+//   if ($ideaTitle.val() === "" || $ideaBody.val() === "") {
+//     alert("Please Enter Fillout The Title And Body Fields");
+//   } else {
+//     userInput();
+//   }
+// }
 
-function userInput() {
+function newIdea() {
   var ideaTitle = $ideaTitle.val();
   var ideaBody = $ideaBody.val();
   var newestIdea = new CardInfo(ideaTitle, ideaBody);
@@ -49,11 +50,12 @@ function CardInfo (title, body) {
 function cardCreater(idea) {
   $anchor.prepend(`<article id=${idea.id} class="card">
                       <h2 class=".title-display">${idea.title}</h2>
-                      <input type="button" name="delete button" class="delete-button" onClick="deleteCard(${idea.id})">
+                      <input type="button" name="delete button" class="delete-button">
                       <p class="card-body">${idea.body}</p>
                       <input type="button" class="arrow-button upvote">
                       <input type="button" class="arrow-button downvote">
-                      <h3 class="quality">quality: <span class="quality-text">swill<span>
+                      <h3 class="quality">quality: 
+                        <span class="quality-text">${idea.quality}</span>
                       </h3>
                     </article>`);
 };
@@ -71,23 +73,33 @@ function recreateCards() {
   }
 }
 
-function deleteCard(id) {
-  $thisArticle = $(`#${id}`);
-  $thisArticle.css('display', 'none');
-  localStorage.removeItem(id);
+$('.card-section').on('click', '.delete-button', deleteCard);
+$('.card-section').on('click', '.upvote', upVote);
+// $('.card-section').on('click', '.downvote', upVote);
+
+
+
+function deleteCard() {
+  this.closest('article').remove();
+  localStorage.removeItem(this.closest('article').id);
 }
 
-$anchor.on('click', function () {
-  console.log('great idea');
-  quality += 1;
-  checkQuality ();
-});
+function upVote() {
+  console.log(this.siblings());
+  var id = this.closest('article').id
+  var ideaObject = JSON.parse(localStorage.getItem(id))
+  if (ideaObject.quality === 'swill') {
+    ideaObject.quality = 'plausible';
+    localStorage.setItem(id,JSON.stringify(ideaObject));
+    $(this).siblings().hasClass('quality-text').text('plausible');
+  }
+};
 
-$anchor.on('click', function () {
-  console.log('terrible idea');
-  quality -= 1;
-  checkQuality ();
-});
+// $anchor.on('click', function () {
+//   console.log('terrible idea');
+//   quality -= 1;
+//   checkQuality ();
+// });
 
 function checkQuality() {
   if (quality === 1) {
