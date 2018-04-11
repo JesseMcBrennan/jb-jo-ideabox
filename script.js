41479f4dbb80076bc87d7754b29eb9f716e17921
Jesse McFadden <jessemcbrennan@gmail.com>
2018-04-11 11:52:33 -0600

    Add blur on enter feature

--- script.js
+++ script.js
@@ -31,12 +31,24 @@ function sortByQuality() {
 // save todo and/or edited text on enter-key press
 
 
-
 function updateBody () {
   var id = this.closest('article').id
   var toDoObject = JSON.parse(localStorage.getItem(id));
   toDoObject.body = $('#' + id.toString() + ' .card-body').text();
   storeObject(toDoObject);
+  if (event.which == 13) {
+    this.blur();
+  }
+}
+
+function updateTitle () {
+  var id = this.closest('article').id
+  var toDoObject = JSON.parse(localStorage.getItem(id));
+  toDoObject.title = $('#' + id.toString() +' .title-display').text();
+  storeObject(toDoObject);
+  if (event.which == 13) {
+    this.blur();
+  }
 }
 
 function toggleCompleted() {
@@ -62,12 +74,6 @@ function showCompleted() {
   }
 }
 
-function updateTitle () {
-  var id = this.closest('article').id
-  var toDoObject = JSON.parse(localStorage.getItem(id));
-  toDoObject.title = $('#' + id.toString() +' .title-display').text();
-  storeObject(toDoObject);
-}
 
 function toggleButton () {  
   if ($('.input-title').val() === "" || $('.input-body').val() === "") {
